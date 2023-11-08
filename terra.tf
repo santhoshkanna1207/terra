d89608f6db3010d72f30407365f30424008f8959
santhoshkanna1207 <147807373+santhoshkanna1207@users.noreply.github.com>
2023-11-08 23:14:39 +0530

    Update terra.tf

--- terra.tf
+++ terra.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "ap-northeast-3"
-  access_key = "AKIASHUFUHFJK4KICMWA"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
 resource "aws_instance" "new" {
   ami           = "ami-01c05ebdecd0ac208"
